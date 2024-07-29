6fa89b90613739a67a0946adcbb62761f804b9f6
dev-rjw <rjw0105@gmail.com>
2024-07-29 14:13:54 +0900

    240729 추가 구현사항

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 const API_KEY = "20f0ecb59c6cafabe067c9e78cc2a42a";
 
 // 영화 리스트 뿌려주기
-function setMovieList(data) {
-    document.querySelector("#cards").innerHTML = "";
+function setMovieList(URL) {
+    fetch(URL)
+        .then((response) => response.json())
+        .then((data) => {
+            document.querySelector("#cards").innerHTML = "";
 
-    data.results.forEach((element) => {
-        let posterPath = "https://image.tmdb.org/t/p/w500" + element["poster_path"];
-        let title = element["original_title"];
-        let overview = element["overview"];
-        let id = element["id"];
-        let rating = element["vote_average"];
+            data.results.forEach((element) => {
+                let posterPath = element["poster_path"] === null ? "https://t4.ftcdn.net/jpg/03/00/32/13/360_F_300321387_gPEMsHdZPAWO2HfqfwEXUfy5MwPvq8AM.jpg" : "https://image.tmdb.org/t/p/w500" + element["poster_path"];
+                let title = element["original_title"];
+                let overview = element["overview"];
+                let id = element["id"];
+                let rating = element["vote_average"];
 
-        const temp = document.createElement("div");
-        temp.className = "col";
-        temp.innerHTML = `
-        <div class="card" onclick="getId(${id})">
-            <img src="${posterPath}" class="card-img-top" alt="..." />
-            <div class="card-body">
-                <h5 class="card-title"><b>${title}</b></h5>
-                <p class="card-text">${overview}</p>
-                <p class="rating">Rating: ${rating}</p>
-            </div>
-        </div>
-        `;
+                const temp = document.createElement("div");
+                temp.className = "col";
+                temp.innerHTML = `
+                <div class="card" onclick="getId(${id})">
+                    <img src="${posterPath}" class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title"><b>${title}</b></h5>
+                        <p class="card-text">${overview}</p>
+                        <p class="rating">Rating: ${rating}</p>
+                    </div>
+                </div>
+                `;
 
-        document.querySelector("#cards").append(temp);
-    });
+                document.querySelector("#cards").append(temp);
+            });
+        })
+        .catch((error) => console.error("Error:", error));
 }
 
 // 영화 클릭 시 아이디값 alert
@@ -36,17 +41,14 @@ function getId(id) {
 // 영화 검색
 function getSearchMovie() {
     const input = document.getElementById("input").value;
-    const URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${input}&language=en-US&page=1`;
-    fetch(URL)
-        .then((response) => response.json())
-        .then((data) => {
-            if (input === "") {
-                alert("빈 값을 입력했습니다.");
-            } else {
-                setMovieList(data);
-            }
-        })
-        .catch((error) => console.error("Error:", error));
+
+    let URL = "";
+    if (input === "") {
+        URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    } else {
+        URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${input}&language=en-US&page=1`;
+    }
+    setMovieList(URL);
 }
 
 window.onload = function () {
@@ -55,12 +57,7 @@ window.onload = function () {
 
     // 영화 목록
     const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
-    fetch(URL)
-        .then((response) => response.json())
-        .then((data) => {
-            setMovieList(data);
-        })
-        .catch((error) => console.error("Error:", error));
+    setMovieList(URL);
 
     // 영화 검색 Enter
     const input = document.getElementById("input");
